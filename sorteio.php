baf6428e9b980ca4416762d20bb407c521febcc0
hamada-minoro <hamada.minoro.0@gmail.com>
2024-12-13 17:28:24 -0300

    quase-finalizado

--- sorteio.php
+++ sorteio.php
@@ -1,7 +1,7 @@
 <?php 
+// Obtém o nome do usuário vindo do POST
 require "db.php";
 require "data.php";
-// Obtém o nome do usuário vindo do POST
 $usuario = $_POST['nome'] ?? null;
 $query_status = "SELECT status_2 FROM participantes WHERE nome = '$usuario'";
 $result_status = mysqli_query($con, $query_status);
@@ -22,7 +22,26 @@ excluirParticipante($con, $nomeSorteado);
 
 usuarioJaSorteou($con, $usuario);
 
-// Exibe o resultado
-echo "O participante sorteado foi: " . $nomeSorteado;
-
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado do Sorteio</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+<body>
+
+<div class="resultado-container">
+    <h1>Resultado do Sorteio</h1>
+    <p>O participante sorteado foi:</p>
+    <div class="nome-sorteado">
+        <?php echo $nomeSorteado; ?>
+    </div>
+    <br>
+    <button class="btn-voltar" onclick="window.history.back()">Voltar</button>
+</div>
+
+</body>
+</html>
